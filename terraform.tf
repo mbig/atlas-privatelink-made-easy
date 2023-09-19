23910e0d6d916486d7364430444bd10603b0ac6b
Gefter Chongong <mbijeff@gmail.com>
2023-09-19 19:28:13 -0400

    adding mongodbatlas provider

--- terraform.tf
+++ terraform.tf
@@ -11,11 +11,12 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-
       version = "~> 3.28.0"
     }
+    mongodbatlas = {
+      source = "mongodb/mongodbatlas"
+    }
   }
-
   required_version = ">= 1.1.0"
 }
 
