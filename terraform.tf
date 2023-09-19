5535b715bfeda4f636265ffdcbd338bb697bdd43
Gefter Chongong <mbijeff@gmail.com>
2023-09-19 19:21:16 -0400

    adding provider region

--- terraform.tf
+++ terraform.tf
@@ -11,9 +11,14 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
+
       version = "~> 3.28.0"
     }
   }
 
   required_version = ">= 1.1.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
 }
